33ac3c33baf9447e276557366d37bae64087360e
uroojisar <uroojisaar@gmail.com>
2021-10-28 15:23:55 +0500

    Added Header(with Logo), Sign in/up page, Form input component (Sign in) & custom button

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import HomePage from './Pages/homepage/homepage.component';
 import './App.css';
 import {Route, Switch} from 'react-router-dom';
 import ShopPage from './Pages/shop/shop.component';
-
+import Header from "./Component/header/header.component";
+import SignInAndSignUpPage from './Pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
       <Route exact path='/' component={HomePage}/>
       <Route path='/shop' component={ShopPage}/>
+      <Route path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
     </div>
   );
---
+++ src/Pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './sign-in-and-sign-up.styles.scss';
+import SignIn from '../../Component/sign-in/sign-in.component';
+
+const SignInAndSignUpPage = () => (
+    <div className="sign-in-and-sign-up"><SignIn/></div>
+);
+
+export default SignInAndSignUpPage;
