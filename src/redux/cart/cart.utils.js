072f6ee7de65a12239a00b906876380cca0e1a22
uroojisar <uroojisaar@gmail.com>
2021-11-05 18:26:19 +0500

    Update CartUtils.js (fixed issue)

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -3,7 +3,7 @@ export const addItemsToCart = (cartItems, cartItemToAdd) => {
     if (existingCartItem) {
         return cartItems.map(cartItem => 
             cartItem.id === cartItemToAdd.id
-            ? { cartItem, quantity: cartItem.quantity + 1} : cartItem);
+            ? { ...cartItem, quantity: cartItem.quantity + 1} : cartItem);
     }
     return [...cartItems, {...cartItemToAdd, quantity: 1 }];
 }
